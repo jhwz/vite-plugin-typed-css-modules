b1582e03abb67bec94970056d16d0133b903ff8f
Joel <joelhowse@gmail.com>
2022-07-20 09:30:57 +1200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import type { Plugin } from "vite";
+import { DtsCreator } from "typed-css-modules/lib/dts-creator.js";
+import fs from "fs";
+
+function plugin(): Plugin {
+  const creater = new DtsCreator({ camelCase: true });
+
+  return {
+    name: "typed-css-modules",
+    config: () => ({
+      css: {
+        modules: {
+          localsConvention: "camelCaseOnly",
+        },
+      },
+    }),
+    configureServer: (server) => {
+      server.watcher.on("change", async (path) => {
+        if (!path.endsWith(".module.css")) return;
+        try {
+          const content = await creater.create(path, undefined, true);
+          await content.writeFile();
+        } catch (e) {
+          /* ignore */
+        }
+      });
+      server.watcher.on("unlink", (path) => {
+        if (!path.endsWith(".module.css")) return;
+        try {
+          fs.unlinkSync(path + ".d.ts");
+        } catch (e) {
+          /* ignore */
+        }
+      });
+    },
+  };
+}
+
+export default plugin;
