97255a04cffed9a59ff0ed1f0addbea85879b031
Joel Howse <joel@helicalco.com>
2024-12-05 08:37:52 +1300

    feat: better extension support

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import type { PluginOption, UserConfig } from "vite";
-import { DtsCreator } from "typed-css-modules/lib/dts-creator.js";
 import fs from "fs";
+import { DtsCreator } from "typed-css-modules/lib/dts-creator.js";
+import type { PluginOption, UserConfig } from "vite";
 
 export type TypedCssModulesOptions = {
-  fileExtension?: `.${string}`;
-}
+  fileExtension?: `.${string}` | `.${string}`[];
+};
 
 function plugin(options?: TypedCssModulesOptions): PluginOption {
   const creater = new DtsCreator({ camelCase: true });
@@ -22,10 +22,17 @@ function plugin(options?: TypedCssModulesOptions): PluginOption {
       return config;
     },
     configureServer: (server) => {
-      const extension = options?.fileExtension ?? '.css';
+      const extensions = !options?.fileExtension
+        ? [".css"]
+        : Array.isArray(options.fileExtension)
+        ? options.fileExtension
+        : [options.fileExtension];
+        
+      const is_css_module = (path: string) =>
+        extensions.some((ext) => path.endsWith(`.module${ext}`));
 
       server.watcher.on("change", async (path) => {
-        if (!path.endsWith(`.module${extension}`)) return;
+        if (!is_css_module(path)) return;
         try {
           const content = await creater.create(path, undefined, true);
           await content.writeFile();
@@ -34,7 +41,7 @@ function plugin(options?: TypedCssModulesOptions): PluginOption {
         }
       });
       server.watcher.on("unlink", (path) => {
-        if (!path.endsWith(`.module${extension}`)) return;
+        if (!is_css_module(path)) return;
         try {
           fs.unlinkSync(path + ".d.ts");
         } catch (e) {
