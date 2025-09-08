ef5d620f0a25a2c524404ccd855056b9dda585e2
Alex / KATT <alexander@n1s.se>
2025-09-08 19:48:36 +0200

    feat: enhance plugin options with include and ignore patterns, add verbose logging, and refactor type definition generation

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,131 @@
-import fs from "fs";
-import { DtsCreator } from "typed-css-modules/lib/dts-creator.js";
-import type { PluginOption, UserConfig } from "vite";
+import fs from 'fs';
+import { DtsCreator } from 'typed-css-modules/lib/dts-creator.js';
+import {
+  createFilter,
+  type FilterPattern, type PluginOption, type UserConfig,
+} from 'vite';
+
+const defaultFilesGlob = '**/*.module.css';
 
 export type TypedCssModulesOptions = {
+  /**
+   * A glob pattern to match the files to scan for CSS modules.
+   * Defaults to any `*.module.css` files in the project.
+   */
+  include?: FilterPattern;
+
+  /**
+   * A glob pattern to match the files to ignore.
+   * @default undefined
+   */
+  ignore?: FilterPattern;
+
+  /**
+   * Enable verbose logging for debugging
+   * @default false
+   */
+  verbose?: boolean;
+
+  /**
+   * @deprecated use {@link TypedCssModulesOptions.include} instead
+   */
   fileExtension?: `.${string}` | `.${string}`[];
 };
 
+function assertUnreachable(value: never): never {
+  throw new Error(`Unreachable value: ${value}`);
+}
+
+function coerceArray<T>(value: T | T[]): T[] {
+  return Array.isArray(value) ? value : [value];
+}
+
 function plugin(options?: TypedCssModulesOptions): PluginOption {
-  const creater = new DtsCreator({ camelCase: true });
+  let include: FilterPattern = options?.include ?? defaultFilesGlob;
+  if (options?.fileExtension) {
+    if (options.include) {
+      throw new Error(`Pick either the \`include\` or \`fileExtension\` option, not both.`);
+    }
+    include = coerceArray(options.fileExtension).map((extension) => `**/*${extension}`);
+  }
+
+  const filter = createFilter(include ?? defaultFilesGlob, options?.ignore);
+  const verbose: boolean = options?.verbose ?? false;
+
+  const creator = new DtsCreator({ camelCase: true });
+
+  function debugLog(message: string) {
+    if (verbose) {
+      // eslint-disable-next-line no-console
+      console.debug(`[typed-css-modules] ${message}`);
+    }
+  }
+  function isCssModule(file: string) {
+    const result = filter(file);
+    debugLog(`[isCssModule] ${file} is ${result ? 'a CSS module' : 'not a CSS module'}`);
+    return result;
+  }
+
+  async function generateTypeDefinitions(file: string) {
+    debugLog(`[generateTypeDefinitions] Generating type definitions for ${file}`);
+    const dts = await creator.create(file, undefined, true);
+    await dts.writeFile();
+    debugLog(`[generateTypeDefinitions] Wrote type definitions at ${dts.outputFilePath}`);
+  }
 
   return {
-    name: "typed-css-modules",
-    config: () => {
+    name: 'typed-css-modules',
+    config() {
       const config: UserConfig = {
         css: {
           modules: {
-            localsConvention: "camelCaseOnly",
+            localsConvention: 'camelCaseOnly',
           },
         },
       };
       return config;
     },
-    configureServer: (server) => {
-      const extensions = !options?.fileExtension
-        ? [".css"]
-        : Array.isArray(options.fileExtension)
-        ? options.fileExtension
-        : [options.fileExtension];
-        
-      const is_css_module = (path: string) =>
-        extensions.some((ext) => path.endsWith(`.module${ext}`));
+    async buildStart(options) {
+      const files = Object.values(options.input).filter(isCssModule);
 
-      server.watcher.on("change", async (path) => {
-        if (!is_css_module(path)) return;
-        try {
-          const content = await creater.create(path, undefined, true);
-          await content.writeFile();
-        } catch (e) {
-          /* ignore */
+      await Promise.all(files.map(generateTypeDefinitions));
+    },
+    async watchChange(file, change) {
+      if (!isCssModule(file)) {
+        debugLog(`[watchChange:${change.event}] Skipping type definitions for ${file} because it does not match files glob`);
+        return;
+      }
+
+      await (async () => {
+        switch (change.event) {
+          case 'create':
+          case 'update': {
+            debugLog(`[watchChange:${change.event}] Generating type definitions for ${file}`);
+
+            await generateTypeDefinitions(file);
+
+            return;
+          }
+          case 'delete': {
+            debugLog(`[watchChange:${change.event}] Deleting type definitions for ${file}`);
+
+            const dtsPath = `${file}.d.ts`;
+
+            if (fs.existsSync(dtsPath)) {
+              fs.unlinkSync(dtsPath);
+            } else {
+              debugLog(`[watchChange:${change.event}] Type definitions for ${file} not found`);
+            }
+
+            return;
+          }
+          default: {
+            assertUnreachable(change.event);
+          }
         }
-      });
-      server.watcher.on("unlink", (path) => {
-        if (!is_css_module(path)) return;
-        try {
-          fs.unlinkSync(path + ".d.ts");
-        } catch (e) {
-          /* ignore */
-        }
+      })().catch((error) => {
+        // eslint-disable-next-line no-console
+        console.error(`[typed-css-modules] [watchChange:${change.event}] Error processing ${file}: ${error}`);
       });
     },
   };
